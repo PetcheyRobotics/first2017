7f51c7c63903d6754f96232a0000d6c2fcbc1353
User <User@10.4.0.252>
2018-02-05 17:42:14 +0000

    Claw Subsystem

--- src/org/usfirst/frc/team5781/robot/subsystems/ClawSubsystem.java
+++ src/org/usfirst/frc/team5781/robot/subsystems/ClawSubsystem.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5781.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,7 +8,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class ClawSubsystem extends Subsystem {
 	
+	DoubleSolenoid claw = new DoubleSolenoid(1,1);
 	
+	public void openClaw() {
+		claw.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	public void closeClaw() {
+		claw.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	public void stop() {
+		claw.set(DoubleSolenoid.Value.kOff);
+	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
